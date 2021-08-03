3e5c01b6091e00a1f8507b7229e0a3b1fb9a9d15
leandrosimonato <leandrosimonato@gmail.com>
2021-08-03 16:13:17 -0300

    Tipando variáveis com typescript.
    Signed-off-by: leandrosimonato <leandrosimonato@gmail.com>

---
+++ 9.2-Tipando-variaveis/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const personName = 'Leandro';
+const personAge = 32;
+const personAlive = true;
+const personContacts = [
+    'leandro.leandrosimonato@gmail.com',
+    0,
+    'leandro.leandrosimonato@gmail.com'
+];
---
+++ 9.2-Tipando-variaveis/index.ts
@@ -0,0 +1,9 @@
+const personName: string = 'Leandro'
+const personAge: number = 32
+const personAlive: boolean = true
+
+const personContacts: (string | number)[] = [
+  'leandro.leandrosimonato@gmail.com',
+  0,
+  'leandro.leandrosimonato@gmail.com'
+]
